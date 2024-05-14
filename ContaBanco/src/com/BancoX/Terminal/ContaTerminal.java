1e5bb93557d00cb3a06d6af9604db1e6fad46d26
Otavio <oaalves@outlook.com.br>
2024-05-14 12:23:33 -0300

    Desafio Sintaxe Final

--- ContaBanco/src/com/BancoX/Terminal/ContaTerminal.java
+++ ContaBanco/src/com/BancoX/Terminal/ContaTerminal.java
@@ -2,7 +2,26 @@ package com.BancoX.Terminal;
 
 import java.util.Scanner;
 
+/**<h1>ContaBanco</h1>
+ * Desafio proposto pelo instrutor de Java <b>Gleyson Sampaio</b>, corresponde á Sintaxe Java.
+ * <p>
+ * <b>Nota:</b>Neste Desafio contém maioria dos Sintaxe java mostrado no treinamento. Sendo elas: <p>
+ * Classes, Tipos de Variáveis, CamelCase, Indicações proibidas em nomes de variáveis, Organização de Arquivos, Concatenação, Java Doc.
+ * 
+ * @author Otávio de Andrade Alves
+ * @version 2.0
+ * @since 14/05/2024
+ * 
+ * @param input Classe. Variável de chamada da classe Scanner, recebendo entrada no terminal do usuario.
+ * @param contaCorrente int. Variável de entrada sobre conta corrente.
+ * @param agenciaBanco String. Variável de entrada sobre agência Bancaria.
+ * @param nomeCompleto String. Variável de entrada sobre Nome completo.
+ * @param saldoConta Double. Variável de entrada com valor Decimal 
+ * @return Não há tipos de Retornos.
+ */
+
 public class ContaTerminal {
+
         public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
